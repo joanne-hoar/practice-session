5ab64836c2d8e38e23089ded94393dd34b2e3433
Joanne Hoar <joanne.hoar@gmail.com>
2026-01-31 17:11:09 -0700

    added new page components

---
+++ src/app/products-home/products-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-home',
+  imports: [],
+  templateUrl: './products-home.html',
+  styleUrl: './products-home.css',
+})
+export class ProductsHome {
+
+}
