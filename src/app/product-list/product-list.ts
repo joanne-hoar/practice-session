9f52fde8fba8eec7bfcd8bd68e7863d353dc4ca8
Joanne Hoar <joanne.hoar@gmail.com>
2026-02-11 16:30:57 -0700

    create a product data service

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
-import { Product } from '../product';
 import { CartService } from '../services/cart-service';
+import { ProductsDataService } from '../services/products-data.service';
 
 @Component({
   selector: 'app-product-list',
@@ -11,57 +11,11 @@ import { CartService } from '../services/cart-service';
 })
 export class ProductList {
   cartService = inject(CartService);
-  allProducts: Product[] = [
-    {
-      id: 1,
-      name: "Can of Beans",
-      image: "can-of-beans.jpg"    
-    },
-    {
-      id: 2,
-      name: "Bag of Crisps",
-      image: "bag-of-crisps.jpg"
-    },
-    {
-      id: 3,
-      name: "Gummy Bears",
-      image: "gummy-bears.jpg"
-    },
-    {
-      id: 4,
-      name: "T-Shirt",
-      image: "t-shirt.jpg"
-    },
-    {
-      id: 5,
-      name: "Hobo Bag",
-      image: "hobo-bag.jpg"
-    },
-    {
-      id: 6,
-      name: "Shoes",
-      image: "shoes.jpg",
-    },
-    {
-      id: 7,
-      name: "Shorts",
-      image: "shorts.jpg"
-    },
-    {
-      id: 8,
-      name: "Postcard",
-      image: "postcard.jpg"
-    },
-    {
-      id: 9,
-      name: "Pens",
-      image: "pens.jpg"
-    }
-  ];
+  productsDataService = inject(ProductsDataService);
 
   // This method is triggered when the child emits the event (see html template)
   receiveAddToCart(id: number) {
-    const product = this.allProducts.find(p => p.id === id);
+    const product = this.productsDataService.getProductById(id);
     if (product) {
       this.cartService.addToCart(product);
     }
---
+++ src/app/services/products-data.service.ts
@@ -0,0 +1,59 @@
+import { Injectable, signal } from '@angular/core';
+import { Product } from '../product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsDataService {
+  products = signal<Product[]>([
+    {
+      id: 1,
+      name: "Can of Beans",
+      image: "can-of-beans.jpg"    
+    },
+    {
+      id: 2,
+      name: "Bag of Crisps",
+      image: "bag-of-crisps.jpg"
+    },
+    {
+      id: 3,
+      name: "Gummy Bears",
+      image: "gummy-bears.jpg"
+    },
+    {
+      id: 4,
+      name: "T-Shirt",
+      image: "t-shirt.jpg"
+    },
+    {
+      id: 5,
+      name: "Hobo Bag",
+      image: "hobo-bag.jpg"
+    },
+    {
+      id: 6,
+      name: "Shoes",
+      image: "shoes.jpg",
+    },
+    {
+      id: 7,
+      name: "Shorts",
+      image: "shorts.jpg"
+    },
+    {
+      id: 8,
+      name: "Postcard",
+      image: "postcard.jpg"
+    },
+    {
+      id: 9,
+      name: "Pens",
+      image: "pens.jpg"
+    }
+  ]);
+
+  getProductById(id: number): Product | undefined {
+    return this.products().find(p => p.id === id);
+  }
+}
